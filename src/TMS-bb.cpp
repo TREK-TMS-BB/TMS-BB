4c447c8c0495eee38c2f17507a2b892c329f9bea
Kristof De Middelaer <kristof.de.middelaer@gmail.com>
2013-11-04 17:42:20 +0100

    Initial project commit.

---
+++ src/TMS-bb.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : TMS-bb.cpp
+// Author      : TREK
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "This is going to be awesome!" << std::endl; // prints This is going to be awesome!
+	return 0;
+}
