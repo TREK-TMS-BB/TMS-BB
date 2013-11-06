f7bb2900659a95116b3624a32a50d9f5037cdf89
RobinVerachtert <robin.verachtert@hotmail.be>
2013-11-06 17:48:35 +0100

    Added CNF_CFG subclass of CFG 	throwt een error als het wordt geinitialiseerd zonder de juiste rules 	heeft een method checkString(string) die true teruggeeft als een 	bepaalde string geaccepteerd wordt door de CFG (gebeurt via CYK algo)

--- src/CFG.cpp
+++ src/CFG.cpp
@@ -103,11 +103,147 @@ CFG::CFG(std::vector<std::string> &v, std::vector<std::string> &t, std::map<std:
 	}
 }
 
-CFG::CFG(std::string file)
-{
+CFG::CFG(std::string file){
 	//TODO: Read File and construct the parameters
 }
 
+CFG::CFG(CFG& copy)
+: variables(copy.variables), terminals(copy.terminals), rules(copy.terminals), startSymbol(copy.startSymbol)
+{}
+
+void CFG::toCNF(){
+	 //TODO: @Erkki: Transform given function to CNF
+}
+
 CFG::~CFG() {
+}
+
+
+CNF_CFG::CNF_CFG() : CFG() {}
+
+CNF_CFG::CNF_CFG(std::vector<std::string> &v, std::vector<std::string> &t, std::map<std::string, std::vector<std::string> > &r, std::string s) : CFG(v, t, r, s)
+{
+	/*
+	 * Check if rules are of the correct form:
+	 */
+
+	for (std::map<std::string, std::vector<std::string> >::iterator r_it = rules.begin(); r_it != rules.end(); r_it++)
+	{
+		if (r_it->second.size() == 1)
+		{
+			bool ter = false;
+			for (std::vector<std::string>::iterator t_it = terminals.begin(); t_it != terminals.end(); t_it++)
+			{
+				if (r_it->second.at(0) == *t_it)
+				{
+					ter = true;
+					break;
+				}
+			}
+			if (ter == false)
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0) << " WHERE "<< r_it->second.at(0) << "IS NOT A TERMINAL. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+		}
+		else if (r_it->second.size() == 2)
+		{
+			bool r1 = false;
+			bool r2 = false;
+			for (std::vector<std::string>::iterator v_it = variables.begin(); v_it != variables.end(); v_it++)
+			{
+				if (r_it->second.at(0) == *v_it)
+				{
+					r1 = true;
+				}
+				else if (r_it->second.at(1) == *v_it)
+				{
+					r2 = true;
+				}
+				if ((r1 == true) && (r2 == true))
+				{
+					break;
+				}
+			}
+			if ((r1 == false) && (r2 == false))
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0)<<r_it->second.at(1) << " WHERE "<< r_it->second.at(0) << " AND " <<r_it->second.at(1) << "ARE NO VARIABLEs. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+			else if (r1 == false)
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0) <<r_it->second.at(1)<< " WHERE "<< r_it->second.at(0) << "IS NOT A VARIABLE. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+			else if (r2 == false)
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0) <<r_it->second.at(1)<< " WHERE "<< r_it->second.at(1) << "IS NOT A VARIABLE. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+		}
+	}
+}
+
+CNF_CFG::CNF_CFG(CFG& cfg) : CFG(cfg)
+{
+	/*
+	 * Check if rules are of the correct form:
+	 */
+
+	for (std::map<std::string, std::vector<std::string> >::iterator r_it = rules.begin(); r_it != rules.end(); r_it++)
+	{
+		if (r_it->second.size() == 1)
+		{
+			bool ter = false;
+			for (std::vector<std::string>::iterator t_it = terminals.begin(); t_it != terminals.end(); t_it++)
+			{
+				if (r_it->second.at(0) == *t_it)
+				{
+					ter = true;
+					break;
+				}
+			}
+			if (ter == false)
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0) << " WHERE "<< r_it->second.at(0) << "IS NOT A TERMINAL. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+		}
+		else if (r_it->second.size() == 2)
+		{
+			bool r1 = false;
+			bool r2 = false;
+			for (std::vector<std::string>::iterator v_it = variables.begin(); v_it != variables.end(); v_it++)
+			{
+				if (r_it->second.at(0) == *v_it)
+				{
+					r1 = true;
+				}
+				else if (r_it->second.at(1) == *v_it)
+				{
+					r2 = true;
+				}
+				if ((r1 == true) && (r2 == true))
+				{
+					break;
+				}
+			}
+			if ((r1 == false) && (r2 == false))
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0)<<r_it->second.at(1) << " WHERE "<< r_it->second.at(0) << " AND " <<r_it->second.at(1) << "ARE NO VARIABLEs. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+			else if (r1 == false)
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0) <<r_it->second.at(1)<< " WHERE "<< r_it->second.at(0) << "IS NOT A VARIABLE. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+			else if (r2 == false)
+			{
+				std::string errorMessage = "Because in rule " << r_it->first << " → " << r_it->second.at(0) <<r_it->second.at(1)<< " WHERE "<< r_it->second.at(1) << "IS NOT A VARIABLE. \n THIS CFG IS NOT IN CHOMSKY NORMAL FORM";
+				throw (errorMessage);
+			}
+		}
+	}
 }
 
--- src/CFG.h
+++ src/CFG.h
@@ -28,6 +28,20 @@ class CFG {
 	 * TODO Constructs a CFG by parsing the description written in the file
 	 */
 	CFG(std::string file);
+
+	/*
+	 * Copy constructor
+	 * copies the atributes from copy to this
+	 */
+	CFG(CFG& copy);
+
+	/*
+	 * Transforms a CFG to Chomsky Normal Form:
+	 * only has translations of the form:
+	 * A → BC or A → a
+	 */
+	void toCNF();
+
 	virtual ~CFG();
 private:
 	std::vector<std::string> variables;
@@ -35,6 +49,39 @@ private:
 	std::map<std::string, std::vector<std::string> > rules;
 	std::string startSymbol;
 
+};
+
+class CNF_CFG : public CFG {
+public:
+	/*
+	 * Default constructor:
+	 * same as CFG
+	 */
+	CNF_CFG();
+
+	/*
+	 * Create a CNF CFG with given parameters
+	 * constructs a CFG and then CNF CFG
+	 * Check if the rules are in the right format
+	 */
+	CNF_CFG(std::vector<std::string> &v, std::vector<std::string> &t, std::map<std::string, std::vector<std::string> > &r, std::string s);
+
+	/*
+	 * Create a CNF based on a CFG
+	 * TODO: 2 Options:
+	 * 		→ Check if the rules are of the right form and throw exception if not
+	 * 		→ Check if the rules are of the right form and run toCNF on cfg and make CNF CFG
+	 */
+	CNF_CFG(CFG& cfg);
+
+	/*
+	 * Check if a given string w is in the CFG
+	 * using the CYK algorythm
+	 */
+	bool check_string(std::string w);
+private:
+	std::map<std::string, bool> checked;	// keeps track of all the strings that have already been checked on this CFG
+	bool checkRules();
 };
 
 #endif /* CFG_H_ */
