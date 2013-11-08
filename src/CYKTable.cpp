16d14642c163211512e3f01888931523f5378285
RobinVerachtert <robin.verachtert@hotmail.be>
2013-11-08 14:18:09 +0100

    Added print to CYK

--- src/CYKTable.cpp
+++ src/CYKTable.cpp
@@ -8,11 +8,50 @@
 #include "CYKTable.h"
 
 CYKTable::CYKTable() {
-	// TODO Auto-generated constructor stub
+	Collumn temp;
+	std::vector<std::string> temp2;
 
+	temp2.push_back("A");
+	temp.push_back(temp2);
+	temp2.push_back("B");
+	temp.push_back(temp2);
+	temp2.push_back("C");
+	temp.push_back(temp2);
 }
 
 CYKTable::~CYKTable() {
 	// TODO Auto-generated destructor stub
 }
 
+std::ostream& operator<<(std::ostream& out, const CYKTable& c) {
+	/*Table::iterator it = c.table.begin();
+	for (Table::iterator it = c.table.begin(); it != c.table.end(); it++)
+	{
+		for (Collumn::iterator c_it = it->begin(); c_it != it->end(); c_it++ )
+		{
+			out << "{";
+			for (TableEntry::iterator e_it = c_it->begin(); e_it != c_it->end(); e_it++)
+			{
+				out << *e_it << ", ";
+			}
+			out << "}" << "\t";
+		}
+		out << std::endl;
+	}*/
+	return out;
+}
+
+std::vector<std::string> CYKTable::at(unsigned int i, unsigned int j) const{
+	int collumn = i-1;
+	int row = (j-i);
+
+	if (row >= table.size())
+	{
+		throw ("ARGUMENT J IS OUT OF BOUNDS");
+	}
+	else if (collumn >= table.at(row).size())
+	{
+		throw ("ARGUMENT I IS OUT OF BOUNDS");
+	}
+	return table.at(row).at(collumn);
+}
--- src/CYKTable.h
+++ src/CYKTable.h
@@ -9,17 +9,25 @@
 #define CYKTABLE_H_
 
 #include <vector>
-typedef std::vector<std::vector<std::vector<std::string> > > Table;
+#include <iostream>
+
+typedef std::vector<std::string> TableEntry;
+typedef std::vector<TableEntry > Collumn;
+typedef std::vector<Collumn > Table;
+
 class CYKTable {
 public:
 	CYKTable();
 	/*
 	 * access item at column i and row j - i + 1
 	 */
-	std::vector<std::string> at(int i, int j);
+	std::vector<std::string> at(unsigned int i, unsigned int j) const;
 	virtual ~CYKTable();
+
+	friend std::ostream& operator<<(std::ostream& out, const CYKTable& c);
 private:
 	Table table;
 };
+
 
 #endif /* CYKTABLE_H_ */
