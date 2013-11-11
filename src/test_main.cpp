326dd1a81e3225c979e5d7cda1f88cacdb268d99
robin <robin@robin-TravelMate-5744>
2013-11-11 10:02:40 +0100

    added test main

---
+++ src/test_main.cpp
@@ -0,0 +1,18 @@
+/*
+ * test_main.cpp
+ *
+ *  Created on: Nov 11, 2013
+ *      Author: robin
+ */
+
+#include <iostream>
+#include "CYKTable.h"
+#include "CFG.h"
+
+
+int main() {
+	std::cout << "TESTS FOR CFG" << std::endl;
+
+	std::cout << "TESTS FOR CYK" << std::endl;
+}
+
