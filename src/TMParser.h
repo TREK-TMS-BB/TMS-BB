a43047dac74568b95bc31c4718cbbd026415c2e2
Kristof De Middelaer <kristof.de.middelaer@gmail.com>
2013-11-24 14:42:04 +0100

    Added documentation for TMParser.

--- src/TMParser.h
+++ src/TMParser.h
@@ -29,27 +29,54 @@ public:
 
 	virtual ~TMParser();
 
+	/**
+	 * @return initial input for the TuringMachine.
+	 */
 	std::string getInitInput() const;
 
+	/**
+	 * @return all states for the TuringMachine.
+	 */
 	std::vector<TM::StateName> getStates() const;
 
+	/**
+	 * @return all productions for the TuringMachine.
+	 */
 	std::vector<TM::Production> getProductions() const;
 
 	friend std::ostream& operator<<(std::ostream& out, TMParser& tmp);
 
 private:
+	//! Initial input for the Turing Machine.
 	std::string initInput_;
 
+	//! All states for the Turing Machine.
 	std::vector<TM::StateName> states_;
 
+	//! All productions for the Turing Machine.
 	std::vector<TM::Production> productions_;
 
+	/**
+	 * @param line The string that contains the states.
+	 * @param found The index where the string "STATES = {" begins.
+	 */
 	void parseStates(std::string, std::size_t);
 
+	/**
+	 * @param line The string that contains the initial input.
+	 * @param found The index where the string "INITIAL_INPUT = " begins.
+	 */
 	void parseInitInput(std::string, std::size_t);
 
+	/**
+	 * @param line The string that contains a production.
+	 */
 	void parseProduction(std::string);
 
+	/**
+	 * @param it2& The iterator we need to use in our check.
+	 * @param c The char the iterator needs to pass.
+	 */
 	void iteratorCheck(std::string::iterator&, char) const;
 };
 
