cd983233d2aaf61de5352138367abfa39c02eb30
robin <robin@robin-TravelMate-5744>
2013-12-09 15:35:01 +0100

    added Tom's Demos

--- src/TMS-bb.cpp
+++ src/TMS-bb.cpp
@@ -13,6 +13,7 @@
 #include "TMParser.h"
 #include "TuringMachine.h"
 #include "LRParser.h"
+#include "PDA.h"
 
 
 int main( int argc, char* argv[]) {
@@ -93,6 +94,8 @@ int main( int argc, char* argv[]) {
 	parser::TMParser test2("TM-Files/faultyExample.tm");
 	std::cout << test2 << std::endl;*/
 
+	//
+	// TM use for development
 
 	TM::TuringMachine t("TM-Files/clear.tm");
 	std::cout << t << std::endl;
@@ -116,6 +119,70 @@ int main( int argc, char* argv[]) {
 	std::cout << "program finished"	 << std::endl;
 
 
+	// =====================================
+	// 			PDA DEMO CHANGETYPE
+	// =====================================
+	/*
+	std::cout << "=================" << std::endl;
+	std::cout << "RUNNING TYPE DEMO" << std::endl;
+	std::cout << "=================" << std::endl << std::endl;
+
+	try {
+		Grammar::CFG Grammar(argv[1]);
+		PDA::PDA pda(&Grammar);
+		std::cout << "- SETUP PDA FROM CFG FILE: " << argv[1] << std::endl;
+
+		pda.toLaTeX("./LaTeX/stackAcceptance1.tex");
+		std::cout << "- CURRENT PDA PRINTED TO LaTeX: ./LaTeX/stackAcceptance1.tex" << std::endl;
+		pda.toFinalStateAcceptance();
+		std::cout << "- CHANGED TO FINAL STATE ACCEPTANCE." << std::endl;
+		pda.toLaTeX("./LaTeX/stateAcceptance.tex");
+		std::cout << "- CURRENT PDA PRINTED TO LaTeX: ./LaTeX/stateAcceptance.tex" << std::endl;
+		pda.toEmptyStackAcceptance();
+		std::cout << "- CHANGED TO EMPTY STACK ACCEPTANCE." << std::endl;
+		pda.toLaTeX("./LaTeX/stackAcceptance2.tex");
+		std::cout << "- CURRENT PDA PRINTED TO LaTeX: ./LaTeX/stackAcceptance2.tex" << std::endl;
+
+		std::cout << std::endl << "========================" << std::endl;
+		std::cout << "PROGRAM FINISHED RUNNING" << std::endl;
+		std::cout << "========================" << std::endl;
+	}
+
+	catch (Exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	*/
+	// =====================================
+	//		  PDA DEMO CONTAINSSTRING
+	// =====================================
+	/*
+	std::cout << "===================" << std::endl;
+	std::cout << "RUNNING STRING DEMO" << std::endl;
+	std::cout << "===================" << std::endl << std::endl;
+
+	try {
+		Grammar::CFG Grammar(argv[1]);
+		PDA::PDA pda(&Grammar);
+		std::cout << "- SETUP PDA FROM CFG FILE: " << argv[1] << std::endl;
+
+		bool contains = pda.containsString(argv[2]);
+		std::cout << "- TESTING WHETHER STRING \"" << argv[2] << "\" IS ACCEPTED." << std::endl;
+		if (contains) {
+			std::cout << "\tThe PDA accepts the string \"" << argv[2] << "\"." << std::endl;
+		}
+		else {
+			std::cout << "\tThe PDA does not accept the string \"" << argv[2] << "\"." << std::endl;
+		}
+
+		std::cout << std::endl << "========================" << std::endl;
+		std::cout << "PROGRAM FINISHED RUNNING" << std::endl;
+		std::cout << "========================" << std::endl;
+	}
+
+	catch (Exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	*/
 
 
 	//CFGParser CFGP("cfg.xml");
