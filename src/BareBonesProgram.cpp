a32715807b153f8766ae5000f225c8c67e652ca8
robin <robin@robin-TravelMate-5744>
2013-12-12 16:38:09 +0100

    added BB

---
+++ src/BareBonesProgram.cpp
@@ -0,0 +1,21 @@
+/*
+ * BareBonesProgram.cpp
+ *
+ *  Created on: Nov 29, 2013
+ *      Author: robin
+ */
+
+#include "BareBonesProgram.h"
+
+namespace BB {
+
+BareBonesProgram::BareBonesProgram() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BareBonesProgram::~BareBonesProgram() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace BB */
---
+++ src/BareBonesProgram.h
@@ -0,0 +1,32 @@
+/*
+ * BareBonesProgram.h
+ *
+ *  Created on: Nov 29, 2013
+ *      Author: robin
+ */
+
+#ifndef BAREBONESPROGRAM_H_
+#define BAREBONESPROGRAM_H_
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <iostream>
+#include"BareBonesStatement.h"
+
+namespace BB {
+
+
+class BareBonesProgram {
+public:
+	BareBonesProgram();
+	virtual ~BareBonesProgram();
+	//! A function writing the BareBones program in TM language to given file
+	void createTMFile(std::string outputFile);
+private:
+	std::vector<std::shared_ptr<BareBonesStatement> > statements; //! all statements of the program
+};
+
+} /* namespace BB */
+
+#endif /* BAREBONESPROGRAM_H_ */
